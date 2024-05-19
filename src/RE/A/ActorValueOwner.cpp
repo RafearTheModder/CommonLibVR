35c74d6a3506ce1e10d48cc838b054aeb9860a62
RafearTheModder <RafearTheModder@gmail.com>
2024-05-19 15:39:38 -0500

    Remove no longer needed ifdef due to addresslibrary update

--- src/RE/A/ActorValueOwner.cpp
+++ src/RE/A/ActorValueOwner.cpp
@@ -19,11 +19,7 @@ namespace RE
 	float ActorValueOwner::GetDualCastingEffectiveness(float a_cost) const
 	{
 		using func_t = decltype(&ActorValueOwner::GetDualCastingEffectiveness);
-#ifdef SKYRIMVR
-		REL::Relocation<func_t> func(REL::Offset(0x3d0f80)); // Remove this ifdef when REL::ID(25931) is added to the VR library
-#else
 		REL::Relocation<func_t> func(RELOCATION_ID(25931, 26518));
-#endif
 		return func(this, a_cost);
 	}
 }
